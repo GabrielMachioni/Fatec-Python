8fb4939b70f188d061bfbe88295d6558513c6014
cesarsilvadev <cesarsilvapedro@gmail.com>
2023-06-05 22:09:40 -0300

    adicionado função sobre

---
+++ utils/sobre.py
@@ -0,0 +1,19 @@
+
+
+def informacao_projeto():
+    print(
+"""
+                   ***********************OBJETIVO***********************
+  O projeto apresentado está utilizando o framework "Django" e o banco de dados "Sqlite".
+
+                   ***********************TEMA***********************
+  Foi escolhido como Tema "Blog de Receitas" tendo como objetivo, contribuir e descobrir novas
+ receitas.
+
+                   ***********************Desenvolvedores***********************
+    César Silva Pedro        RA: 2840481921015
+    Gabriel Gomes Machioni   RA: 2840482021014
+"""
+    )
+
+informacao_projeto()
