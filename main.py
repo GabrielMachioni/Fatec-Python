9eb2a2fe04715cc777946024c19be70bbb791006
GabrielMachioni <gabrielmachioni@gmail.com>
2023-06-07 17:56:19 -0300

    feat: add novas funcoes

--- main.py
+++ main.py
@@ -1,39 +1,94 @@
-import os
+import sys
 from time import sleep
 
-from usuario.cadastro import criar_cadastro
-from utils.sobre import informacao_projeto
+from utils import telas
+from utils.funcoes_auxiliares import printc, limpar_tela
+from usuario import cadastro, login
 
-while True:
-    print(
-"""
-1 - Login
-2 - Cadastrar
-3 - Sobre
+def main():
+    """
+    Tela principal
+    """
+    limpar_tela()
+    op = int(input(telas.menu_principal))
+    while op not in [1, 2, 8, 9]:
+        limpar_tela()
+        printc('vermelho', 'Opção inválida, tente novamente: ')
+        op = int(input(telas.menu_principal))
 
-9 - Sair
-""")
+    if op == 1:
+        fazer_login()
 
-    op = int(input('Escolha uma opção: '))
+    elif op == 2:
+        fazer_cadastro()
+    
+    elif op == 8:
+        mostrar_sobre()
 
-    if op in [1, 2, 3, 9]:
-        break
+    else:
+        printc('verde', 'Saindo...')
+        sleep(1)
+        sys.exit()
+
+
+def fazer_login():
+    """
+    Fazer Login do Usuário
+    """
+    limpar_tela()
+    login_user = login.Login()
+
+    login_user.get_infos()
+
+    user_logado()
+
+
+def fazer_cadastro():
+    """
+    Fazer Cadastro do Usuário
+    """
+    limpar_tela()
+    new_user = cadastro.Cadastro()
+
+    printc('amarelo', telas.menu_cadastro)
+
+    new_user.get_informations()
+    sleep(1)
+    main()
+
+
+def mostrar_sobre():
+    """
+    Mostrar tela Sobre
+    """
+    limpar_tela()
+    print(telas.tela_sobre)
+
+
+def user_logado():
+    limpar_tela()
     
-    os.system('cls')
-    print('Opção Inválida!')
-    sleep(2)
+    op = int(input(telas.menu_login))
+    while op not in [1, 2, 8, 9]:
+        limpar_tela()
+        printc('vermelho', 'Opção inválida, tente novamente: ')
+        op = int(input(telas.menu_login))
+
+    if op == 1:
+        ...
+
+    elif op == 2:
+        ...
     
+    elif op == 8:
+        ...
+
+    else:
+        printc('verde', 'Saindo...')
+        sleep(1)
+        sys.exit()
     
-if op == 1:
-    #login()
-    ...
-    
-elif op == 2:
-    criar_cadastro()
-    
-elif op == 3:
-    informacao_projeto()
-    
-else:
-    exit()
-    
+
+if __name__ == '__main__':
+    main()
+
